770fd6aed2dfb1bfb07454cf9e44ce0591d5a050
eugene <tubavek@ethersportz.info>
2020-03-08 21:52:36 -0700

    KE: Make build debug only and pull required repos

--- dev-build-all.sh
+++ dev-build-all.sh
@@ -11,17 +11,30 @@
 # cmake -D sets a variable in the cmake cache (here I set the build config for the specific makefile that gets created)
 # https://cmake.org/cmake/help/v3.2/manual/cmake.1.html
 
+# Get dependency if they are not in place
+if [ ! "$(ls -A deps/cotire)" ] 
+then
+	echo "Installing cotire"
+	git clone https://github.com/sakra/cotire deps/cotire
+fi
+
+if [ ! "$(ls -A deps/googletest)" ] 
+then
+	echo "Installing googletest"
+	git clone https://github.com/google/googletest deps/googletest
+fi
+
 echo "BUILDING DEBUG CONFIG..."
 mkdir -p build/Unix-x64/Debug && cd build/Unix-x64/Debug && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ../../.. && make && cd ../../..
 
-echo "BUILDING MINSIZEREL CONFIG..."
-mkdir -p build/Unix-x64/MinSizeRel && cd build/Unix-x64/MinSizeRel && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel ../../.. && make && cd ../../..
+#echo "BUILDING MINSIZEREL CONFIG..."
+#mkdir -p build/Unix-x64/MinSizeRel && cd build/Unix-x64/MinSizeRel && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=MinSizeRel ../../.. && make && cd ../../..
 
-echo "BUILDING RELEASE CONFIG..."
-mkdir -p build/Unix-x64/Release && cd build/Unix-x64/Release && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../.. && make && cd ../../..
+#echo "BUILDING RELEASE CONFIG..."
+#mkdir -p build/Unix-x64/Release && cd build/Unix-x64/Release && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../../.. && make && cd ../../..
 
-echo "BUILDING RELWITHDEBINFO CONFIG..."
-mkdir -p build/Unix-x64/RelWithDebInfo && cd build/Unix-x64/RelWithDebInfo && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo ../../.. && make && cd ../../..
+#echo "BUILDING RELWITHDEBINFO CONFIG..."
+#mkdir -p build/Unix-x64/RelWithDebInfo && cd build/Unix-x64/RelWithDebInfo && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo ../../.. && make && cd ../../..
 
 # pause the script at the end, until user wants to close it (analog to DOS-pause)
 # https://stackoverflow.com/questions/92802/what-is-the-linux-equivalent-to-dos-pause
