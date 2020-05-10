2cb845afef06643937ecee23e50fa5f61c6b2cdb
Yassine Missoum <ymissoum1@gmail.com>
2020-05-09 17:38:29 -0700

    YM - Added filesystem and logging library and fixed small changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include <fstream>
 #include <iostream>
-#include <filesystem>
 
 // defines
-#define JSONPATH "assets\\main.json"
+#define JSONPATH "./assets/main.json"
 
 #include "simulate.io/pch.h"
 #include "utils/json.hpp"
 #include "utils/string-utils.hpp"
+#include "utils/log/loguru.cpp"
+#include "utils/filesystem.hpp"
 
 #include "simulate.io/game/ICharacter.h"
-#include "simulate.io/game/IBattle.h"
 #include "simulate.io/game/WarriorDummy.h"
 #include "simulate.io/game/MeleeBattle.h"
 
 // namespaces
-using json = nlohmann::json;
-namespace fs = std::filesystem;
+using json 	 = nlohmann::json;
+namespace fs = ghc::filesystem;
 
 int main(int argc, char* argv[]) {
 
-	WarriorDummy atacker;
+	// YM: Initializing log library
+	loguru::init(argc, argv);
+	loguru::add_file("simulate.io.txt", loguru::Append, loguru::Verbosity_MAX);
+	LOG_F(INFO, "Starting simulate.io");
+
+	WarriorDummy attacker;
 	WarriorDummy defender;
-	MeleeBattle battle(std::move(atacker), std::move(defender));
+	MeleeBattle battle(std::move(attacker), std::move(defender));
 
 	battle.BeginFight();
 	battle.LogResults();
@@ -31,11 +36,14 @@ int main(int argc, char* argv[]) {
 	std::error_code error;
 	bool bExists = fs::exists(JSONPATH, error);
 
-	assert(bExists == true && "assets\\main.json not found!");
+	assert( bExists && "./assets/main.json not found!");
 
 	std::ifstream stream(JSONPATH);
 	json main_json = json::parse(stream);
-	std::cout << "Version: " << main_json["v_simulate.io"] << std::endl;
+
+    std::string version = main_json["v_simulate.io"];
+
+	LOG_F(INFO,"Version:%s" , version.data());
 
 	return 0;
 }
